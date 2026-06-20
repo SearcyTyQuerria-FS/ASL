02ad9e494db0cefd98970a80cc5a24eb58886369
SearcyTyQuerria-FS <tasearcy@student.fullsail.edu>
2026-06-19 20:07:53 -0400

    setup database models and successful migration

---
+++ models/planet.js
@@ -0,0 +1,30 @@
+"use strict";
+const { Model } = require("sequelize");
+module.exports = (sequelize, DataTypes) => {
+  class Planet extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+      Planet.belongsToMany(models.Star, {
+        through: models.StarsPlanets,
+        foreignKey: "planetId",
+      });
+    }
+  }
+  Planet.init(
+    {
+      Name: DataTypes.STRING,
+      Size: DataTypes.INTEGER,
+      Description: DataTypes.TEXT,
+    },
+    {
+      sequelize,
+      modelName: "Planet",
+    },
+  );
+  return Planet;
+};
---
+++ models/star.js
@@ -0,0 +1,27 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Star extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+      Star.belongsTo(models.Galaxy, {foreignKey: 'galaxyId'});
+      Star.belongsToMany(models.Planet, {through: models.StarsPlanets, foreignKey: 'starId'});
+    }
+  }
+  Star.init({
+    Name: DataTypes.STRING,
+    Size: DataTypes.INTEGER,
+    Description: DataTypes.TEXT
+  }, {
+    sequelize,
+    modelName: 'Star',
+  });
+  return Star;
+};
